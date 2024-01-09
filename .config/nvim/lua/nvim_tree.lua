8c520ebdf615fc8aadcaf7fa8a14a8712eacd90f
khaled Eldoheiri <khaled@eldoheiri.com>
2024-01-09 19:39:37 +0100

    feat: toggle nvim-tree show

--- .config/nvim/lua/nvim_tree.lua
+++ .config/nvim/lua/nvim_tree.lua
@@ -9,5 +9,7 @@ local function open_nvim_tree(data)
   vim.cmd.cd(data.file)
   -- open the tree
   require("nvim-tree.api").tree.open()
+
 end
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+vim.keymap.set('n', '<Leader>tt', '<Cmd>NvimTreeToggle<Cr>', {remap=true})
