9776ea3deed7935d00f7e88d2173dfb1ced96233
khaled Eldoheiri <khaled@eldoheiri.com>
2024-06-29 19:42:23 +0200

    feat (nvim): set auto formatting on terraform buf write

--- nvim/lua/terraform.lua
+++ nvim/lua/terraform.lua
@@ -2,5 +2,11 @@ require('lsp_config')
 
 -- terraform language server
 require('lspconfig').terraformls.setup {
-	on_attach = On_attach
+	on_attach = function(client, bufnr)
+		On_attach(client, bufnr)
+		vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+			pattern = { "*.tf", "*.tfvars" },
+			callback = vim.lsp.buf.format({ async = false }),
+		})
+	end,
 }
